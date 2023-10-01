e6111232efed24d421516d56b6c4236fe09d4bb0
Protsak Petro <49090552+ProtsakPetro@users.noreply.github.com>
2023-10-01 06:18:22 +0200

    fix_app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,35 +1,35 @@
-import { Component } from "react";
+import React, { Component } from "react";
 import FeedbackOptions from "./FeedbackOptions/FeedbackOptions";
 import Section from "./Section/Section";
 import Statistics from "./Statistics/Statistics";
 import { Container } from "./index.styled";
 import Notification from "./Notification/Notification";
 
-export class App extends Component {
+class App extends Component {
   state = {
     LIKE: 0,
     NEUTRAL: 0,
     DISLIKE: 0
-  }
-  
+  };
+
   countTotalFeedback = () => {
     const { LIKE, NEUTRAL, DISLIKE } = this.state;
     return LIKE + NEUTRAL + DISLIKE;
-  }
-  
+  };
+
   countPositiveFeedbackPercentage = () => {
-    const { LIKE, DISLIKE } = this.state;
+    const { LIKE } = this.state;
     const total = this.countTotalFeedback();
     const percentage = total === 0 ? 0 : (LIKE / total) * 100;
     return Math.round(percentage) + "%";
-  }
+  };
 
-  hendlerFeedback = (e) => {
+  handleFeedback = (e) => {
     const buttonName = e.target.textContent;
     this.setState((prevState) => ({
       [buttonName]: prevState[buttonName] + 1
     }));
-  }
+  };
 
   render() {
     const { LIKE, NEUTRAL, DISLIKE } = this.state;
@@ -38,7 +38,7 @@ export class App extends Component {
         <Section title="SHARE YOUR EXPERIENCES">
           <FeedbackOptions
             options={Object.keys(this.state)}
-            onLeaveFeedback={this.hendlerFeedback}
+            onLeaveFeedback={this.handleFeedback}
           />
         </Section>
         <Section title="STATISTIC">
@@ -47,8 +47,8 @@ export class App extends Component {
               LIKE={LIKE}
               NEUTRAL={NEUTRAL}
               DISLIKE={DISLIKE}
-              total={this.countTotalFeedback}
-              positivePercentage={this.countPositiveFeedbackPercentage}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
             />
           ) : (
             <Notification message="THERE IS NO FEEDBACK" />
@@ -58,3 +58,5 @@ export class App extends Component {
     );
   }
 }
+
+export default App;
